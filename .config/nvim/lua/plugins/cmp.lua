a0877df791c9a3130f82debe78a73c111d99e9ff
Fisokuhle Magagula <fisokuhlemagagula13@gmail.com>
2026-01-08 12:32:03 +0200

    Update cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,35 +1,31 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-		},
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
-		end,
-	},
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets", -- optional but recommended for snippets
+      "L3MON4D3/LuaSnip",
+    },
+    opts = {
+      keymap = { preset = 'default' },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+
+      windows = {
+        autocomplete = {
+          border = "single", -- or "rounded", "double", "shadow"
+        },
+        documentation = {
+          border = "single",
+        },
+        signature = {
+          border = "single",
+        },
+      },
+
+      snippets = {
+        preset = "luasnip",
+      },
+    },
+  }
 }
