39f4d240f05afa7f945ad6ab896462d3f87afd4f
Fisokuhle Magagula <fisokuhlemagagula13@gmail.com>
2026-03-11 09:32:05 +0200

    Adjust colors

--- .config/nvim/lua/themes/nord.lua
+++ .config/nvim/lua/themes/nord.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     config = function()
       vim.g.nord_disable_background = true
-      vim.g.nord_borders = false
+      vim.g.nord_borders = true
 
       vim.o.termguicolors = true
       vim.cmd([[colorscheme nord]])
@@ -15,6 +15,9 @@ return {
       vim.api.nvim_set_hl(0, "PmenuSel", { bg = "none", bold = true })
       vim.api.nvim_set_hl(0, "CmpItemAbbr", { bg = "none" })
       vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "none", bold = true })
+      vim.api.nvim_set_hl(0, "FloatBorder", { link = "Type" })
+
+      vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#4C566A", bg = "none" })
     end,
   },
 }
