a1fa94ff113fdb01ce10900d643e398828fbbd6f
Fisokuhle Magagula <fisokuhlemagagula13@gmail.com>
2026-04-06 10:37:24 +0200

    remove background - make it transparent

--- .config/nvim/lua/themes/solarized-osaka.lua
+++ .config/nvim/lua/themes/solarized-osaka.lua
@@ -15,6 +15,43 @@ return {
       vim.o.termguicolors = true
       vim.cmd([[colorscheme solarized-osaka]])
 
+
+      -- utility: inherit highlight, remove only background
+      local function remove_bg(group)
+        local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = group, link = false })
+        if not ok or not hl then return end
+
+        hl.bg = nil
+        vim.api.nvim_set_hl(0, group, hl)
+      end
+
+
+      local groups = {
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NeoTreeWinSeparator",
+        "NeoTreeEndOfBuffer",
+
+        -- Telescope
+        "TelescopeBorder",
+        "TelescopeNormal",
+        "TelescopePrompt",
+        "TelescopePromptNormal",
+        "TelescopePromptBorder",
+        "TelescopePromptTitle",
+        "TelescopeResultsNormal",
+        "TelescopeResultsBorder",
+        "TelescopeResultsTitle",
+        "TelescopePreviewNormal",
+        "TelescopePreviewBorder",
+        "TelescopePreviewTitle",
+      }
+
+      for _, group in ipairs(groups) do
+        remove_bg(group)
+      end
+
+
       require("themes.utils.utils").custom_italic()
     end,
   },
